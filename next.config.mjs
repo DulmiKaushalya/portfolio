230dc71e3381096010af1afb6da61b09c02bff9e
DulmiKaushalya <146383047+DulmiKaushalya@users.noreply.github.com>
2025-02-03 16:55:43 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // This tells Next.js to generate a static export
+    distDir: 'out', // Specifies the output directory
+    images: {
+      unoptimized: true, // Fix for images in static export
+    },
+  };
+  
+  export default nextConfig;
+  
